395e087b88a8e037d59f538caa88198de84fa317
Michael Langford <mchllngfrd771@gmail.com>
2017-01-15 04:28:57 -0600

    -A

---
+++ ngApp/services/stuffServices.ts
@@ -0,0 +1,18 @@
+export class Product {
+  public name:string;
+  public price:number;
+}
+
+export function doSomething() {
+  console.log('did something!');
+}
+
+function doSomethingSecret() {
+  console.log('secret!');
+}
+
+export default function doSomethingElse() {
+    console.log('something else!');
+}
+
+export const orange = '#FFA500';
